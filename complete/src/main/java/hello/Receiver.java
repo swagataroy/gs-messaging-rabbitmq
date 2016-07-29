89200d30ff135a4682f32c1b01be587096f767fb
swagatar <sroy@pivotal.io>
2016-07-29 14:56:05 -0400

    swagata updating receiver message#

--- complete/src/main/java/hello/Receiver.java
+++ complete/src/main/java/hello/Receiver.java
@@ -7,7 +7,7 @@ public class Receiver {
 	private CountDownLatch latch = new CountDownLatch(1);
 
 	public void receiveMessage(String message) {
-		System.out.println("Received <" + message + ">");
+		System.out.println("Swagata Received <" + message + ">");
 		latch.countDown();
 	}
 
